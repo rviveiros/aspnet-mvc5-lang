c3c4c6a148230a733a07927b840c667b86636e33
Rafael Viveiros <eu@rafa.lv>
2014-03-12 11:53:17 -0300

    initial commit

---
+++ aspnet-mvc5-lang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aspnet_mvc5_lang.Startup))]
+namespace aspnet_mvc5_lang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
